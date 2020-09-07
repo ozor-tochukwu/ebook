68dd26b711b431a3d8ca08756256afcce967a777
ozor-tochukwu <68199852+ozor-tochukwu@users.noreply.github.com>
2020-09-07 17:17:17 +0100

    Updates

---
+++ precache-manifest.76309401a24a71d4db70d6d8733b6d30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c9ae05a56633713998b4bd17aed3f5",
+    "url": "/ebook/index.html"
+  },
+  {
+    "revision": "042f93226d4547a094ca",
+    "url": "/ebook/static/css/main.4d18448c.chunk.css"
+  },
+  {
+    "revision": "f0cdd26211ee14b91d93",
+    "url": "/ebook/static/js/2.7b2a6fb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ebook/static/js/2.7b2a6fb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "042f93226d4547a094ca",
+    "url": "/ebook/static/js/main.488ecebf.chunk.js"
+  },
+  {
+    "revision": "015511a0c75372517504",
+    "url": "/ebook/static/js/runtime-main.df484a23.js"
+  },
+  {
+    "revision": "6d5c1649598fa95e223a3cb64cb99dee",
+    "url": "/ebook/static/media/cart.6d5c1649.jpg"
+  },
+  {
+    "revision": "6dbc3ecf7b7b6e6ebec19825d66a687a",
+    "url": "/ebook/static/media/unn.6dbc3ecf.jpg"
+  }
+]);
